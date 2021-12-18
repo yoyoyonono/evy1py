49f274dfba4efefdff106f11fd12168aec649de6
Aaron Thapa <notsmart98@gmail.com>
2021-12-17 19:49:45 -0500

    update router

--- router.py
+++ router.py
@@ -1,16 +1,23 @@
+from typing import Iterator
 import mido
 from util import *
 
-input_port = choose_midi_input_port()
+input_port: Iterator[mido.Message] = choose_midi_input_port()
 output_port = choose_midi_output_port()
 
-phonemes = phoneme_to_midi_message(input('Phoneme?')) # see evy1 midi implementation appendix
-output_port.send(phonemes)
+channel = int(input('Channel?'))
+if channel == 0: 
+    phonemes = phoneme_to_midi_message(input('Phoneme?')) # see evy1 midi implementation appendix
+    print(phonemes, phonemes.hex(), sep='\t')
+    output_port.send(phonemes)
 
 try:
     for x in input_port:
         if x.type != 'clock':
-            print(x)
+            x.channel = channel
+            if hasattr(x, 'velocity'):
+                x.velocity = 127
+            print(x, x.hex(), sep='\t')
         output_port.send(x)
 except KeyboardInterrupt:
     input_port.close()
